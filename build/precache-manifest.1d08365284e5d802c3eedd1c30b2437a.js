a77eb3ae8b1c828647301626f14c00ea2fa41ecf
nicolasfabbro-meli <nicolas.fabbro@mercadolibre.com>
2020-03-30 18:35:50 -0300

    add build

---
+++ build/precache-manifest.1d08365284e5d802c3eedd1c30b2437a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4fdb243f27bbb0b3366c50c4c59882",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9f738b9ab2796c8180b",
+    "url": "/static/css/main.7b2dbea0.chunk.css"
+  },
+  {
+    "revision": "f1ea567cf841c0ed448e",
+    "url": "/static/js/2.aa0b90ff.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.aa0b90ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9f738b9ab2796c8180b",
+    "url": "/static/js/main.0c211d97.chunk.js"
+  },
+  {
+    "revision": "4dbed8ab34d4d5755ddd",
+    "url": "/static/js/runtime-main.906ebcfb.js"
+  }
+]);
